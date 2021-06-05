c28916f7cd0be0f94fec341b581d630451a01604
LongNTBui <buingocthanglong@yahoo.com>
2021-06-05 20:59:12 +1000

    first test passed

--- test/simple-cdk-app.test.ts
+++ test/simple-cdk-app.test.ts
@@ -5,9 +5,36 @@ import * as SimpleCdkApp from '../lib/simple-cdk-app-stack';
 test('Empty Stack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new SimpleCdkApp.SimpleCdkAppStack(app, 'MyTestStack');
+    const stack = new SimpleCdkApp.SimpleCdkAppStack(app, 'MySimpleAppBucketNameExport');
     // THEN
     expectCDK(stack).to(matchTemplate({
-      "Resources": {}
+        "Resources": {
+            "MySimpleAppBucket6B59014A": {
+                "Type": "AWS::S3::Bucket",
+                "Properties": {
+                    "BucketEncryption": {
+                        "ServerSideEncryptionConfiguration": [
+                            {
+                                "ServerSideEncryptionByDefault": {
+                                    "SSEAlgorithm": "AES256"
+                                }
+                            }
+                        ]
+                    }
+                },
+                "UpdateReplacePolicy": "Retain",
+                "DeletionPolicy": "Retain"
+            }
+        },
+        "Outputs": {
+            "MySimpleAppBucketNameExport": {
+                "Value": {
+                    "Ref": "MySimpleAppBucket6B59014A"
+                },
+                "Export": {
+                    "Name": "MySimpleAppBucketName"
+                }
+            }
+        }
     }, MatchStyle.EXACT))
 });
