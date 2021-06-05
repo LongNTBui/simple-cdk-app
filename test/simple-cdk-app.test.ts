338e7547a7744499796d8752c2a66df0c187341a
LongNTBui <buingocthanglong@yahoo.com>
2021-06-05 21:10:12 +1000

    Stack create a S3 bucket

--- test/simple-cdk-app.test.ts
+++ test/simple-cdk-app.test.ts
@@ -1,6 +1,7 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as SimpleCdkApp from '../lib/simple-cdk-app-stack';
+import "@aws-cdk/assert/jest";
 
 test('Empty Stack', () => {
     const app = new cdk.App();
@@ -38,3 +39,12 @@ test('Empty Stack', () => {
         }
     }, MatchStyle.EXACT))
 });
+
+test("Stack create a S3 bucket", () => {
+    // ARRANGE
+    const app = new cdk.App();
+    // ACT
+    const stack = new SimpleCdkApp.SimpleCdkAppStack(app, "MyTestStack")
+    // ASSERT
+    expect(stack).toHaveResource('AWS::S3::Bucket');
+})
